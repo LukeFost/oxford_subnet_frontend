ddd9c6f73988499b6f1f00a65573ed9c6cf53b0b
SupremeSeymour <lafoste4@asu.edu>
2024-03-09 11:39:07 -0700

    website yo!

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Next - MetaMask -Template
 
 ![Connect Wallet](./public/walletconnect.png)
+LukeFost found from [Web3_StartKits!](https://www.useweb3.xyz/starter-kits)
 
 This is a [Next.js](https://nextjs.org/) and [MetaMask](https://docs.metamask.io/wallet/how-to/connect/set-up-sdk/javascript/react/) template with [Tailwind](https://tailwindcss.com/) and [ShadcnUI](https://ui.shadcn.com/).
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,48 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import { createWalletClient, custom } from "viem";
+import { mainnet } from "viem/chains";
+
+const walletClient = createWalletClient({
+  transport: custom(window.ethereum),
+});
 
 export default function Home() {
+  const [accounts, setAccounts] = useState<string[] | string>();
+
+  useEffect(() => {
+    const fetchAccounts = async () => {
+      try {
+        // Request user's addresses from MetaMask
+        const accountsList = await walletClient.requestAddresses();
+        setAccounts(accountsList);
+      } catch (error) {
+        console.error("Failed to fetch accounts:", error);
+        setAccounts("Error fetching accounts");
+      }
+    };
+
+    fetchAccounts();
+  }, []);
+
+  const handleConnectWallet = async () => {
+    try {
+      // Request user's addresses from MetaMask
+      const accountsList = await walletClient.requestAddresses();
+      setAccounts(accountsList);
+    } catch (error) {
+      console.error("Failed to connect wallet:", error);
+      setAccounts("Error connecting wallet");
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <button onClick={handleConnectWallet}>Connect Wallet</button>
+      <p>
+        Accounts: {Array.isArray(accounts) ? accounts.join(", ") : accounts}
+      </p>
+      <p>Balance:</p>
+    </main>
   );
 }
